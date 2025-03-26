6cd19ac1519d24b19793a206ca2d7c433f5f2693
sthaicha <67160033@kmitl.ac.th>
2025-03-26 16:46:55 +0700

    Add files via upload

---
+++ cell03/ex04/ex03bis/todo.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    loadTasks();
+
+    $("#new-task").click(function() {
+        let task = prompt("Enter a new task:");
+        if (task) {
+            addTask(task);
+            saveTasks();
+        }
+    });
+
+    function addTask(task) {
+        let div = $("<div></div>").text(task).addClass("task");
+        div.click(function() {
+            if (confirm("Do you want to remove this task?")) {
+                $(this).remove();
+                saveTasks();
+            }
+        });
+        $("#ft_list").prepend(div);
+    }
+
+    function saveTasks() {
+        let tasks = [];
+        $("#ft_list div").each(function() {
+            tasks.push($(this).text());
+        });
+        document.cookie = "tasks=" + encodeURIComponent(JSON.stringify(tasks)) + "; path=/";
+        
+    }
+
+    function loadTasks() {
+        let cookies = document.cookie.split("; ");
+        let taskCookie = cookies.find(row => row.startsWith("tasks="));
+        if (taskCookie) {
+            let tasks = JSON.parse(decodeURIComponent(taskCookie).split("=")[1]);
+            
+            tasks.reverse().forEach(task => addTask(task));
+        }
+    }
+});
